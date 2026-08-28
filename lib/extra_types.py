935fac668458e83b82df6bd3142e7be8175e82f9
whatbee <gabe.bf@gmail.com>
2026-08-28 15:14:12 +0100

    refactor enum

--- lib/extra_types.py
+++ lib/extra_types.py
@@ -1,12 +1,18 @@
 from pydantic import BaseModel, Field
+from enum import Enum
 
 
 class EnvironmentalInfo(BaseModel):
     mouse: tuple[int,int] = Field(description="the location of the user's mouse")
 
+class Action(Enum):
+    move_to = "move_to"
+    idle = "idle"
+    swim_fast = "swim_fast"
+
 class PetAction(BaseModel):
     thought: str = Field(description="The thought process of the pet.")
-    action: str = Field(description="The action to take.", json_schema_extra={"enum": ["move_to", "idle", "swim_fast"]})
+    action: Action = Field(description="The action to take.")
     target_x: int = Field(description="Target X coordinate.")
     target_y: int = Field(description="Target Y coordinate.")
 
@@ -14,7 +20,7 @@ class PetAction(BaseModel):
         return self.thought if self.thought else ""
 
     def get_action(self) -> str:
-        return self.action if self.action else "idle"
+        return self.action.value if self.action else "idle"
 
     def get_target_x(self, default=0.0) -> float:
         ret = self.target_x if self.target_x is not None else default
