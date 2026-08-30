673a6f7ade18438b42e6fe9e18c92086987824a1
whatbee <gabe.bf@gmail.com>
2026-08-30 01:33:11 +0100

    add more iteration testing

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from lib.brain import Brain
 from lib.tank import Tank
 from lib.extra_types import SimulationConfig
+from lib.utils import get_logger
 from models.download import Model, get_model
 
 model_path = get_model(Model.smollm2)
+logger = get_logger(__name__)
 
 RUNTIME = 300
 
@@ -11,8 +13,12 @@ if __name__ == "__main__":
     config = SimulationConfig.model_construct()
     config.tank.runtime = RUNTIME
 
-    for seed in range(1, 10):
-        brain = Brain(model_path, config.brain)
-        brain.config.params.seed = seed
-        simulation = Tank(brain, config.tank)
-        simulation.run()
+    for temp in range(15, 25, 3):
+        config.brain.params.temperature = temp/10
+        for seed in range(1, 7):
+            logger.info(f"{temp=}, {seed=}")
+            config.brain.params.seed = seed
+
+            brain = Brain(model_path, config.brain)
+            simulation = Tank(brain, config.tank)
+            simulation.run()
