17458f59729a4593e1a46d4d221f25050c8dd4b6
whatbee <gabe.bf@gmail.com>
2026-09-03 23:07:58 +0100

    refactor into class makes iterating easier

--- bayes_optimise.py
+++ bayes_optimise.py
@@ -17,91 +17,92 @@ from models.download import Model, get_model
 
 RUNTIME = 300
 N_TRIALS = 25
-N_SEEDS = 3
-
-VERSION = 5
-COMMENTS = ""
+N_SEEDS = 1
 
 logger = get_logger(__name__, "debug", log_file="log.txt")
-model_path = get_model(Model.smollm2)
 
-study_name = f"v{VERSION}_pet_llm_{model_path.stem}"
-report_path = Path(__file__).parent / f"reports/{study_name}.json"
+class Optimiser:
 
-loss_function_weights = LossFunctionWeights(
+    VERSION = 6
+    COMMENTS = ""
+
+    loss_function_weights = LossFunctionWeights(
     thought_loop=10.0,
     empty_thought=10.0,
     out_of_bounds=10.0,
     malformed_json=100.0,
     invalid_chars=10.0,
-)
+    )
 
-tuner_config = TunerConfig(
-    temperature=(0.8, 2.5),
-    frequency_penalty=(0.2, 2.5),
-    presence_penalty=(0.2, 2.5),
-    repeat_penalty=(0.2, 2.5),
-)
+    tuner_config = TunerConfig(
+        temperature=(0.8, 2.5),
+        frequency_penalty=(0.2, 2.5),
+        presence_penalty=(0.2, 2.5),
+        repeat_penalty=(0.2, 2.5),
+    )
 
 
-# TODO: move into `optimisation_helpers`
-def evaluate_simulation(trial: optuna.Trial) -> float:
-    """Evaluates simulation loss for a set of sampled brain parameters.
+    def __init__(self, model: Model) -> None:
+        self.model = model
 
-    Args:
-        trial: An Optuna trial instance used to sample hyperparameters.
+        self.model_path = get_model(self.model)
+        self.study_name = f"v{self.VERSION}_pet_llm_{self.model_path.stem}"
+        self.report_path = Path(__file__).parent / f"reports/{self.study_name}.json"
 
-    Returns:
-        The scalar loss output calculated from the simulation result.
-    """
-    config = SimulationConfig.model_construct()
-    config.tank.runtime = RUNTIME
-    config.brain.params = suggest_vals(trial, tuner_config, config.brain.params)
+    def evaluate_simulation(self, trial: optuna.Trial) -> float:
+        config = SimulationConfig.model_construct()
+        config.tank.runtime = RUNTIME
+        config.brain.params = suggest_vals(trial, self.tuner_config, config.brain.params)
 
-    losses: list[float] = []
+        losses: list[float] = []
 
-    if not report_path.exists():
-        study_report = StudyReport(
-            comments=COMMENTS,
-            tuner_config=tuner_config,
-            loss_function_weights=loss_function_weights,
-            simulation_config=config,
-            trials=[],
+        if not self.report_path.exists():
+            study_report = StudyReport(
+                comments=self.COMMENTS,
+                tuner_config=self.tuner_config,
+                loss_function_weights=self.loss_function_weights,
+                simulation_config=config,
+                trials=[],
+            )
+            self.report_path.write_text(study_report.model_dump_json(indent=2))
+
+        for seed in range(N_SEEDS):
+            config.brain.params.seed = seed
+            brain = Brain(self.model_path, config.brain)
+            tank = Tank(brain, config.tank)
+
+            # make headless:
+            # tank._render_scene = lambda: None  # type: ignore[attr-defined, method-assign]
+
+            vals = [f"{k}={v}" for k, v in config.brain.params]
+            logger.info(f"{seed=}, {', '.join(vals)}")
+            result = tank.run()
+            loss = loss_function(result.report, self.loss_function_weights)
+            logger.info(f"{loss=}")
+            losses.append(loss)
+            append_report(self.report_path, result.report, config.brain.params)
+
+        return sum(losses) / len(losses)
+
+    def run(self):
+        logger.info("starting optimisation")
+        logger.info(f"{RUNTIME=}, {N_TRIALS=}, {N_SEEDS=}")
+        eta = RUNTIME * N_TRIALS * N_SEEDS
+
+        logger.info(f"eta: {humanize.naturaltime(eta, future=True)}")
+
+        study = optuna.create_study(
+            study_name=self.study_name,
+            storage=storage,
+            direction="minimize",
+            load_if_exists=True,
         )
-        report_path.write_text(study_report.model_dump_json(indent=2))
+        study.optimize(self.evaluate_simulation, n_trials=N_TRIALS, show_progress_bar=True)
 
-    for seed in range(N_SEEDS):
-        brain = Brain(model_path, config.brain)
-        tank = Tank(brain, config.tank)
-
-        # make headless:
-        # tank._render_scene = lambda: None  # type: ignore[attr-defined, method-assign]
-
-        vals = [f"{k}={v}" for k, v in config.brain.params]
-        logger.info(f"{seed=}, {', '.join(vals)}")
-        result = tank.run()
-        loss = loss_function(result.report, loss_function_weights)
-        logger.info(f"{loss=}")
-        losses.append(loss)
-        append_report(report_path, result.report, config.brain.params)
-
-    return sum(losses) / len(losses)
+        logger.info(f"Best parameters: {study.best_params}")
+        logger.info(f"Best loss: {study.best_value}")
 
 
 if __name__ == "__main__":
-    logger.info("starting optimisation")
-    logger.info(f"{RUNTIME=}, {N_TRIALS=}, {N_SEEDS=}")
-    eta = RUNTIME * N_TRIALS * N_SEEDS
-
-    logger.info(f"eta: {humanize.naturaltime(eta, future=True)}")
-
-    study = optuna.create_study(
-        study_name=study_name,
-        storage=storage,
-        direction="minimize",
-        load_if_exists=True,
-    )
-    study.optimize(evaluate_simulation, n_trials=N_TRIALS)
-
-    logger.info(f"Best parameters: {study.best_params}")
-    logger.info(f"Best loss: {study.best_value}")
+    opt = Optimiser(Model.miniCPM)
+    opt.run()
