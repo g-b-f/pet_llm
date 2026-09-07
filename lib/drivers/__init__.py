2e38cf8866e70709733c82712f30d3a3decfb2c8
whatbee <gabe.bf@gmail.com>
2026-09-07 19:08:19 +0100

    add lazy load

--- lib/drivers/__init__.py
+++ lib/drivers/__init__.py
@@ -3,3 +3,4 @@ from lib.drivers.dummy import DummyDriver
 from lib.drivers.pygame_driver import PyGameDriver
 
 __all__ = ["DriverBase", "DummyDriver", "PyGameDriver"]
+__lazy_modules__ = ["PyGameDriver"]
--- lib/inference/__init__.py
+++ lib/inference/__init__.py
@@ -2,3 +2,4 @@ from lib.inference.base import InferenceBase
 from lib.inference.llama_cpp_python import LlamaCppPython
 
 __all__ = ["InferenceBase", "LlamaCppPython"]
+__lazy_modules__ = ["LlamaCppPython"]
