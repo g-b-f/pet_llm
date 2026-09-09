4577f79d94cca7bc89807eb835c34507eb1c2da4
whatbee <gabe.bf@gmail.com>
2026-09-09 18:13:04 +0100

    add/ improve mock classes

--- tests/mocks.py
+++ tests/mocks.py
@@ -1,54 +1,100 @@
 from pathlib import Path
+from random import randint
 
+from lib.brain import Brain
 from lib.inference.base import InferenceBase
 from lib.types.config import BrainConfig
 from lib.types.other import PetAction, RoleContent
-from lib.brain import Brain
 
-# TODO: swap things round so __init__ is passed `PetAction`,
-# and there's also a `from_thoughts` method
+
 class ScriptedInference(InferenceBase):
-    def __init__(self, thoughts: list[RoleContent] | RoleContent):
-        if not isinstance(thoughts, list): thoughts = [thoughts]
+    def __init__(self, actions: list[PetAction] | PetAction):
+        if not isinstance(actions, list):
+            actions = [actions]
+        thoughts = [RoleContent.assistant(a.model_dump_json()) for a in actions]
         self.thoughts = iter(thoughts)
 
     @classmethod
-    def from_actions(cls, actions: list[PetAction] | PetAction):
-        if not isinstance(actions, list): actions = [actions]
-        return cls( [RoleContent.assistant(a.model_dump_json()) for a in actions] )
+    def from_thoughts(cls, thoughts: list[RoleContent] | RoleContent):
+        if not isinstance(thoughts, list):
+            thoughts = [thoughts]
+        obj = cls([])
+        obj.thoughts = iter(thoughts)
+        return obj
+
+    @classmethod
+    def infinite(cls, max_target = 100):
+        """Returns an inference object that will infinitely give valid responses"""
+        obj = cls([])
+
+        def response():
+            i = 0
+            while True:
+                target_x = target_y = randint(0, max_target)
+                thought = f"thought {i}"
+                i += 1
+                action = PetAction(thought=thought, target_x=target_x, target_y=target_y)
+                yield RoleContent.assistant(action.model_dump_json())
+
+        obj.thoughts = response()
+
+        return obj
 
     def create_chat_completion(self, *args, **kwargs):
         return next(self.thoughts)
 
+
 class MockInference(InferenceBase):
     """Returns an (invalid) chat completion, regardless of args"""
-    def __init__(self): pass
+
+    def __init__(self):
+        pass
+
     def create_chat_completion(self, *args, **kwargs):
         return RoleContent.assistant("")
 
-class MockValidInference(InferenceBase):
+
+class MockValidInference(MockInference):
     """Same as MockInference, but guaranteed to be valid"""
-    def __init__(self):
-        pass
+
     def create_chat_completion(self, *args, **kwargs):
         return RoleContent.assistant(
             PetAction(thought="hello", target_y=10, target_x=10).model_dump_json()
         )
 
+class InfiniteBrain(Brain):
+    def __init__(self,
+        model_path=Path("fake/path/model.gguf"),
+        config=BrainConfig.model_construct(),
+    ):
+        super().__init__(model_path, config, ScriptedInference.infinite())
+
 class BlockingBrain(Brain):
     """returns inference until the iterator is empty, then blocks with is_thinking=True"""
 
     def __init__(
-            self,
-            thoughts: list[RoleContent] | RoleContent,
-            model_path = Path("fake/path/model.gguf"),
-            config = BrainConfig.model_construct(),
-            ):
-        super().__init__(model_path, config, ScriptedInference(thoughts))
+        self,
+        actions: list[PetAction] | PetAction,
+        model_path=Path("fake/path/model.gguf"),
+        config=BrainConfig.model_construct(),
+    ):
+        super().__init__(model_path, config, ScriptedInference(actions))
+
+    @classmethod
+    def from_thoughts(
+        cls,
+        thoughts: list[RoleContent] | RoleContent,
+        model_path=Path("fake/path/model.gguf"),
+        config=BrainConfig.model_construct(),
+    ):
+        if not isinstance(thoughts, list):
+            thoughts = [thoughts]
+        obj = cls([], model_path, config)
+        obj.inference = ScriptedInference.from_thoughts(thoughts)
+        return obj
 
     def _generate_decision(self, current_x: int, current_y: int):
         try:
             super()._generate_decision(current_x, current_y)
         except StopIteration:
             self.is_thinking = True
-    
